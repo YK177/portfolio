996f934a0f1222eba931da3d515759184acb39b4
YK177 <kotenev.yuri@gmail.com>
2022-01-06 19:57:38 +0400

    - icon font added;

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import style from './Main.module.scss'
 import avatar from '../../assets/images/avatar.png'
+import {Title} from '../common/Title/Title'
 
 export const Main = () => {
     return (
         <section className={style.main}>
             <div className={`container ${style.main__content}`}>
                 <div className={style.main__body}>
+                    <Title>Introduction</Title>
                     <h1 className={style.main__mainTitle}>
                         Hi there! <br/>
                         I'm Yuri Kotenev <br/>
@@ -21,7 +23,6 @@ export const Main = () => {
                         src={avatar}
                         alt="main"/>
                 </div>
-
             </div>
         </section>
     )
